fce1e26848224ea3178e398b95a46c132cc8e4ce
Raymond Oeta <oetaray@gmail.com>
2023-08-03 11:47:43 +0300

    completed work

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,6 +4,17 @@ function Form(props) {
   const [firstName, setFirstName] = useState("Sylvia");
   const [lastName, setLastName] = useState("Woods");
 
+  function handleSubmit(e) => {
+    e.preventDefault()
+    const formData = {
+      firstName: firstName,
+      lastName: lastName
+    }
+    props.sendFormData(formData)
+    setFirstName("")
+    setLastName("")
+  }
+
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
   }
@@ -12,41 +23,33 @@ function Form(props) {
     setLastName(event.target.value);
   }
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    const formData = { firstName: firstName, lastName: lastName}
-    const dataArray = [...submittedData, formData]
-    setSubmittedData(dataArray)
-    setFirstName("")
-    setLastName("")
-  }
-  const [errors, setErrors] = useState([]);
+ 
+ // const [errors, setErrors] = useState([]);
 
-function handleSubmit(event) {
-  event.preventDefault();
-  // first name is required
-  if (firstName.length > 0) {
-    const formData = { firstName: firstName, lastName: lastName };
-    const dataArray = [...submittedData, formData];
-    setSubmittedData(dataArray);
-    setFirstName("");
-    setLastName("");
-    setErrors([]);
-  } else {
-    setErrors(["First name is required!"]);
-  }
-}
+//function handleSubmit(event) {
+//  event.preventDefault();
+//  // first name is required
+//  if (firstName.length > 0) {
+//    const formData = { firstName: firstName, lastName: lastName };
+//    const dataArray = [...submittedData, formData];
+//    setSubmittedData(dataArray);
+//    setFirstName("");
+//    setLastName("");
+// //   setErrors([]);
+//  } else {
+//    setErrors(["First name is required!"]);
+//  }
+//}
 
  
-
-  const listOfSubmissions = submittedData.map((data, index) => {
-    return (
-          <div key={index}>
-            <p>First Name: {data.firstName}</p>
-            <p>Last Name: {data.lastName}</p>
-          </div>
-          )
-  })
+//const listOfSubmissions = submittedData.map((data, index) => {
+   // return (
+        //  <div key={index}>
+        //    <p>First Name: {data.firstName}</p>
+         //   <p>Last Name: {data.lastName}</p>
+         // </div>
+         // )
+  //})
   
   return (
     <div>
@@ -54,15 +57,7 @@ function handleSubmit(event) {
       <input type="text" onChange={handleFirstNameChange} value={firstName}></input>
       <input type="text" onChange={handleLastNameChange} value={lastName}></input>
       <button type="submit">Submit</button>
-      </form>
-      {errors.length > 0 ? errors.map((error, index) =>{
-        <p key={index} style={{ color: "red"}}>
-          {error}
-        </p>
-      })
-      : null}
-      <h3>Submissions</h3>
-      {listOfSubmissions}
+      </form>//
     </div>
   )
 }
